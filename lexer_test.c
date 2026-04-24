c902c49a1aa5f44ad54709c660cd3ab7c0102dab
big-yogurt <245382579+big-yogurt@users.noreply.github.com>
2026-04-24 11:19:55 +0500

    feat: lexer tests rewrited

--- lexer_test.c
+++ lexer_test.c
@@ -5,8 +5,9 @@
 
 #include "darr.h"
 #include "lexer.h"
+#include "utils.h"
 
-bool test_pair(const char* code, TokenKind* toks)
+bool test_pair(const char* code, Token* toks)
 {
     FILE* fd = fmemopen((void*) code, strlen(code), "r");
     panic_if(NULL == fd, "%s", strerror(errno));
@@ -14,12 +15,15 @@ bool test_pair(const char* code, TokenKind* toks)
     Lexer l = Lexer_new(fd);
 
     for (size_t i = 0; ; ++i) {
-        TokenKind t1 = toks[i];
-        TokenKind t2 = Lexer_next_tok(&l).kind;
-        if (t1 != t2) {
+        Token t1 = toks[i];
+        Token t2 = Lexer_next_tok(&l);
+        if (t1.kind != t2.kind) {
             return false;
         }
-        if (TokenKind_EOF == t1 || TokenKind_ERR == t1) {
+        if (t1.diff != t2.diff) {
+            return false;
+        }
+        if (TokenKind_EOF == t1.kind || TokenKind_ERR == t1.kind) {
             break;
         }
     }
@@ -28,47 +32,63 @@ bool test_pair(const char* code, TokenKind* toks)
 }
 
 #define test(name, ...) \
-    printf("[%s] %s\n", test_pair(__VA_ARGS__) ? "PASSED" : "FAILED", name)
+    do { \
+        bool test_result = test_pair(__VA_ARGS__); \
+        printf("[%s] %s\n", test_result ? "PASSED" : "FAILED", name); \
+        if (!test_result) { \
+            exit(-1); \
+        } \
+    } while (0)
 
 int main(void)
 {
     test(
         "+-<>[],.",
         "+-<>[],.",
-        (TokenKind[]) {
-            TokenKind_PLUS,
-            TokenKind_MINUS,
-            TokenKind_L_AB,
-            TokenKind_R_AB,
-            TokenKind_L_B,
-            TokenKind_R_B,
-            TokenKind_COMMA,
-            TokenKind_DOT,
-            TokenKind_EOF,
+        (Token[]) {
+            {.kind = TokenKind_L_B, .diff = 1},
+            {.kind = TokenKind_R_B, .diff = 1},
+            {.kind = TokenKind_COMMA, .diff = 1},
+            {.kind = TokenKind_DOT, .diff = 1},
+            {.kind = TokenKind_EOF, .diff = 1},
         }
     );
 
     test(
         "+-<>[//test\\n],.",
         "+-<>[//test\n],.",
-        (TokenKind[]) {
-            TokenKind_PLUS,
-            TokenKind_MINUS,
-            TokenKind_L_AB,
-            TokenKind_R_AB,
-            TokenKind_L_B,
-            TokenKind_R_B,
-            TokenKind_COMMA,
-            TokenKind_DOT,
-            TokenKind_EOF,
+        (Token[]) {
+            {.kind = TokenKind_L_B, .diff = 1},
+            {.kind = TokenKind_R_B, .diff = 1},
+            {.kind = TokenKind_COMMA, .diff = 1},
+            {.kind = TokenKind_DOT, .diff = 1},
+            {.kind = TokenKind_EOF, .diff = 1},
         }
     );
 
     test(
         "/+-<>[],.",
         "/+-<>[],.",
-        (TokenKind[]) {
-            TokenKind_ERR,
+        (Token[]) {
+            {.kind = TokenKind_ERR, .diff = 1},
+        }
+    );
+
+    test(
+        "..",
+        "..",
+        (Token[]) {
+            {.kind = TokenKind_DOT, .diff = 2},
+            {.kind = TokenKind_EOF, .diff = 1},
+        }
+    );
+
+    test(
+        ",,",
+        ",,",
+        (Token[]) {
+            {.kind = TokenKind_COMMA, .diff = 2},
+            {.kind = TokenKind_EOF, .diff = 1},
         }
     );
 
